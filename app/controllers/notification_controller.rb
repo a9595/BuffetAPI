c3f339a1d490fa065b16af8c4f34e406397fbff3
Andrii Kovalchuk <tieorange@gmail.com>
2016-12-21 16:33:24 +0100

    sends json correctly

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -9,14 +9,13 @@ class NotificationController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    # request.body.rewind
-
     body = request.body.read
-    p "body= \n" + body
+    p "body= " + body
 
-    push_json = JSON.parse body
+#    push_json = JSON.parse body
+    push_json = ""
 
-    # p "push_json= \n" + push_json
+# p "push_json= \n" + push_json
 
     send_notification(body, push_json)
 
@@ -41,11 +40,10 @@ class NotificationController < ApplicationController
   end
 
   def send_notification(body, parsedJson)
-    p "body = " + body
 
     deviceTokenEmulator = "fs5O5HoGKok:APA91bGnj2dKlQ0QfFnCtRU9LvhQyTbWnnw5PFLqzR6emb7r1hTCuZiD6CnLJ7ntu9wts-hZXBSkjH8A1n6bsTHqzmeyznN9pbwO1I88co-GPKsrT5F_X6P3hguA1QcZzZTVWYddXqac"
-    deviceTokenPixel = "cvF-4BFKeKg:APA91bGsoIWgn9Y4psuYsY0Jf0tJMEq9UCsHPpdOyLIFjud49J4BtHqZB02OcAS6Qc0H85aVuR-VtduZH3VFJn2Ro8eA1Whs6R4X8bQLT4yyY1IqrSDqtwzOC7vbqtixf_q8ZhLM_IdJ"
-    deviceToken5X = "cuxGnFB9fVQ:APA91bEBeLrDGdn2be1dGqsZb3_WRNIEkgSd0Tyn3nhuCkFTl6Ba7EYTkzDlwBrIexNddvtoC0QmjtZrM-nyD0niIXn8BkLnpV2uvLIU9Un5uaqBW3KMS0XvbWeXO2tZof7g0xTVYDqr"
+    deviceTokenPixel = "c8MxKBXI4sI:APA91bEGF0ux03XYkVKqQ4x7xDWMsIJc04IMj1tbj72UdQzZtMQeauy2Yibl8hT4b3ozOWgOKDsp2FFjbYnUI3FmCIUfLLXiUBDnLWEfafJzb0ffAm88jeFRVGAYZMA7jmrfapSvHjtY"
+    deviceToken5X = "cvmUMcMamrQ:APA91bHcvSV2xDUjqYT48g_CI3lxelIGtRwDbZrhkCVlj7L6Lrnn3pn9GlaQMT2CKRsMjyWAEsvaxD5skbLktpEuc8h7zMrES5vVK49LQy1zmwfArV6qMkKsq6oRnF1sSZT0nWlMbC33"
     serverKey = "AAAA-Qaz-e8:APA91bHXGHIbaxoAYzSY0fWPsDA3fvNC9x0_Xa1ent9H9jYbYX-EciOOE3RrHGwMaLg_NeZ96hdZmH_H56uSgEFtNwsVZgQpZ03X-O3o3V5PMwHtHPPq37iDmNfvddBa6HDIk9gOWmGg74gHX0wg-snMas5RvFt0LA"
 
     # Extract params:
@@ -60,10 +58,12 @@ class NotificationController < ApplicationController
     mobile_tokens = [deviceTokenPixel, deviceTokenEmulator, deviceToken5X]
     # mobile_tokens = [tokenParam]
     # options = { "data": " body ", collapse_key: "updated_score" }
+
+    # body = body.gsub('\\"', '')
+
     options = { :notification => {
       "title": "Your meal is waiting for you",
       "body": body
-
     } }
     response = fcm.send(mobile_tokens, options)
     p response
